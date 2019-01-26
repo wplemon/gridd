f5ab1b405d834f8a6f6a5324415119816dfe9462
Ari <aristath@gmail.com>
2019-01-26 23:34:19 +0200

    remove custom admin notice for Kirki

--- inc/admin.php
+++ inc/admin.php
@@ -27,22 +27,8 @@ class Admin {
 			return;
 		}
 
-		// Add Kirki-installation nag.
-		add_action( 'admin_notices', [ $this, 'kirki_notice' ] );
-
 		// Add VC styles.
 		add_action( 'admin_footer', [ $this, 'vc_styles' ] );
-	}
-
-	/**
-	 * The kirki notice.
-	 *
-	 * @access public
-	 * @since 1.0
-	 * @return void
-	 */
-	public function kirki_notice() {
-		include get_template_directory() . '/inc/admin/templates/html-notice-kirki.php';
 	}
 
 	/**
