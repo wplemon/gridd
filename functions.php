15fbf3e1eafff0abbae8e8ad047b94f80fa3cfdc
Ari <aristath@gmail.com>
2019-02-01 21:06:49 +0200

    Gracefully fail if requirements are not met. Props @justintadlock

--- functions.php
+++ functions.php
@@ -2,32 +2,17 @@
 /**
  * Gridd functions and definitions
  *
- * @link https://developer.wordpress.org/themes/basics/theme-functions/
- *
  * @package Gridd
  */
 
 /**
- * If the user is on an old PHP version, throw a error in the dashboard
- * so that they may change themes.
- * After that, early exit and don't continue bootstrapping the theme.
+ * Gracefully fail if the user is on an old PHP version
+ * or if using an old version of WordPress.
  */
-if ( version_compare( PHP_VERSION, '5.6.0' ) < 0 ) {
-	/**
-	 * Admin notice for PHP version.
-	 * NON dismissable because if the user doesn't take any action their site won't work.
-	 *
-	 * @since 1.0
-	 */
-	function gridd_admin_notice_php_version() {
-		echo '<div class="notice notice-error"><p style="font-size:16px">';
-		_e( '<strong>ERROR</strong>: The Gridd Theme requires a PHP version greater than 5.6. Please visit <a href="https://wordpress.org/support/update-php/" rel="nofollow">this link</a> for instructions on how to update your PHP version, or switch to a different theme.', 'gridd' ); // phpcs:ignore WordPress.Security.EscapeOutput
-		echo '</p></div>';
-	}
-	add_action( 'admin_notices', 'gridd_admin_notice_php_version' );
-} else {
-	/**
-	 * All good, we're on a recent-ish PHP version. Proceed.
-	 */
-	require_once get_template_directory() . '/inc/bootstrap.php';
+if ( version_compare( $GLOBALS['wp_version'], '4.9.6', '<' ) || version_compare( PHP_VERSION, '5.6', '<' ) ) {
+	require_once get_template_directory() . '/inc/bootstrap-compatibility.php';
+	return;
 }
+
+// Bootstrap the theme.
+require_once get_template_directory() . '/inc/bootstrap.php';
---
+++ inc/bootstrap-compatibility.php
@@ -0,0 +1,99 @@
+<?php
+/**
+ * WP and PHP compatibility.
+ *
+ * This file was copied from the Mythic theme
+ * and tweaked for use in the Gridd theme.
+ *
+ * Functions used to gracefully fail when a theme doesn't meet the minimum WP or
+ * PHP versions required. Note that only code that will work on PHP 5.2.4 should
+ * go into this file. Otherwise, it'll break on sites not meeting the minimum
+ *
+ * @package   Gridd
+ * @author    Justin Tadlock <justintadlock@gmail.com>
+ * @copyright 2018 Justin Tadlock
+ * @license   https://www.gnu.org/licenses/gpl-2.0.html GPL-2.0-or-later
+ * @link      https://themehybrid.com/themes/mythic
+ */
+
+// Add actions to fail at certain points in the load process.
+add_action( 'after_switch_theme', 'gridd_switch_theme' );
+add_action( 'load-customize.php', 'gridd_load_customize' );
+add_action( 'template_redirect', 'gridd_preview' );
+
+/**
+ * Returns the compatibility messaged based on whether the WP or PHP minimum
+ * requirement wasn't met.
+ *
+ * @since  1.0.0
+ * @return string
+ */
+function gridd_compat_message() {
+	if ( version_compare( $GLOBALS['wp_version'], '4.9.6', '<' ) ) {
+		return sprintf(
+			/* Translators: 1 is the required WordPress version and 2 is the user's current version. */
+			esc_html__( 'Gridd requires at least WordPress version %1$s. You are running version %2$s. Please upgrade and try again.', 'gridd' ),
+			'4.9.6',
+			$GLOBALS['wp_version']
+		);
+	}
+
+	if ( version_compare( PHP_VERSION, '5.6', '<' ) ) {
+		return sprintf(
+			/* Translators: 1 is the required PHP version and 2 is the user's current version. */
+			esc_html__( 'Mythic requires at least PHP version %1$s. You are running version %2$s. Please upgrade and try again.', 'gridd' ),
+			'5.6',
+			PHP_VERSION
+		);
+	}
+
+	return '';
+}
+
+/**
+ * Switches to the previously active theme after the theme has been activated.
+ *
+ * @since  1.0.0
+ * @param string $old_name Previous theme name/slug.
+ * @return void
+ */
+function gridd_switch_theme( $old_name ) {
+
+	switch_theme( $old_name ? $old_name : WP_DEFAULT_THEME );
+	unset( $_GET['activated'] );
+	add_action( 'admin_notices', 'gridd_upgrade_notice' );
+}
+
+/**
+ * Outputs an admin notice with the compatibility issue.
+ *
+ * @since  1.0.0
+ * @return void
+ */
+function gridd_upgrade_notice() {
+
+	printf( '<div class="error"><p>%s</p></div>', esc_html( gridd_compat_message() ) );
+}
+
+/**
+ * Kills the loading of the customizer.
+ *
+ * @since  1.0.0
+ * @return void
+ */
+function gridd_load_customize() {
+	wp_die( esc_html( gridd_compat_message() ), '', array( 'back_link' => true ) );
+}
+
+/**
+ * Kills the customizer previewer on installs prior to WP 4.7.
+ *
+ * @since  1.0.0
+ * @access public
+ * @return void
+ */
+function gridd_preview() {
+	if ( isset( $_GET['preview'] ) ) { // WPCS: CSRF ok.
+		wp_die( esc_html( gridd_compat_message() ) );
+	}
+}
