087c822467bdc9387f293fe7878800262596ac71
Ari <aristath@gmail.com>
2019-02-08 01:19:41 +0200

    add templates for download_category and download_tag taxonomies

---
+++ taxonomy-download_category.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * The template for displaying archive pages
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-hierarchy/
+ *
+ * @package Gridd
+ */
+
+get_template_part( 'archive-download' );
