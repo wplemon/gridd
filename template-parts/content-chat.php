e08c55cf9ff2798a7a037602e21175ca63d98c7e
Ari <aristath@gmail.com>
2019-03-02 14:39:51 +0200

    fix typo

--- template-parts/content-chat.php
+++ template-parts/content-chat.php
@@ -12,7 +12,7 @@ $parts = gridd_get_post_parts();
 <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
 	<?php
 	foreach ( $parts as $part ) {
-		if ( in_array( $part, [ 'post-tile', 'post-thumbnail', 'post-content', 'post-category', 'post-tags', 'post-date-author', 'part-post-comments-link' ], true ) ) {
+		if ( in_array( $part, [ 'post-title', 'post-thumbnail', 'post-content', 'post-category', 'post-tags', 'post-date-author', 'part-post-comments-link' ], true ) ) {
 			$part_name = is_archive() ? 'archive' : null;
 			$part_name = is_singular() ? 'singular' : null;
 			gridd_get_template_part( 'template-parts/part-' . $part, $part_name );
