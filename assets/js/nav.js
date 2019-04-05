0b47e8c3d596b600c1c4629a7fc82e94203da777
Ari <aristath@gmail.com>
2019-04-06 02:08:14 +0300

    Make nav work after partials have been loaded

--- assets/js/nav.js
+++ assets/js/nav.js
@@ -1,20 +1,27 @@
 /**
  * Handle togglers.
  */
-Array.prototype.forEach.call( document.querySelectorAll( '.gridd-toggle' ), function( el ) {
-	el.addEventListener( 'click', function( e ) {
-		e.preventDefault();
 
-		// Toggle the "toggled-on" class.
-		el.classList.toggle( 'toggled-on' );
+griddInitNavToggle = function() {
+	Array.prototype.forEach.call( document.querySelectorAll( '.gridd-toggle' ), function( el ) {
+		el.addEventListener( 'click', function( e ) {
+			e.preventDefault();
 
-		// Toggle aria-expanded.
-		if ( el.classList.contains( 'toggled-on' ) ) {
-			el.setAttribute( 'aria-expanded', 'true' );
-		} else {
-			el.setAttribute( 'aria-expanded', 'false' );
-		}
-	}, window.griddSupportsPassive ? {
-		passive: true
-	} : false );
+			// Toggle the "toggled-on" class.
+			el.classList.toggle( 'toggled-on' );
+
+			// Toggle aria-expanded.
+			if ( el.classList.contains( 'toggled-on' ) ) {
+				el.setAttribute( 'aria-expanded', 'true' );
+			} else {
+				el.setAttribute( 'aria-expanded', 'false' );
+			}
+		}, window.griddSupportsPassive ? {
+			passive: true
+		} : false );
+	});
+};
+griddInitNavToggle();
+document.addEventListener( 'griddRestPart', function() {
+    griddInitNavToggle();
 });
