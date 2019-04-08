66ceb659b44ef51aab85e8d7d3d3ab5cb130a539
Ari <aristath@gmail.com>
2019-04-08 23:30:11 +0300

    Add product-searchform template for WC

---
+++ woocommerce/product-searchform.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * The template for displaying product search form
+ *
+ * @see     https://docs.woocommerce.com/document/template-structure/
+ * @package Gridd
+ * @version 3.3.0
+ */
+
+if ( ! defined( 'ABSPATH' ) ) {
+	exit;
+}
+
+?>
+<form role="search" method="get" class="search-form" action="<?php echo esc_url( home_url( '/' ) ); ?>">
+	<label>
+		<span class="screen-reader-text"><?php echo esc_html_x( 'Search for:', 'label', 'gridd' ); ?></span>
+		<input type="search" class="search-field" placeholder="<?php echo esc_attr_x( 'Search products&hellip;', 'placeholder', 'gridd' ); ?>" value="<?php echo esc_attr( get_search_query() ); ?>" name="s" />
+		<svg class="gridd-search-icon" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><path d="M23.809 21.646l-6.205-6.205c1.167-1.605 1.857-3.579 1.857-5.711 0-5.365-4.365-9.73-9.731-9.73-5.365 0-9.73 4.365-9.73 9.73 0 5.366 4.365 9.73 9.73 9.73 2.034 0 3.923-.627 5.487-1.698l6.238 6.238 2.354-2.354zm-20.955-11.916c0-3.792 3.085-6.877 6.877-6.877s6.877 3.085 6.877 6.877-3.085 6.877-6.877 6.877c-3.793 0-6.877-3.085-6.877-6.877z"/></svg>
+	</label>
+	<span class="screen-reader-text">
+		<input type="hidden" name="post_type" value="product" />
+		<input type="submit" class="search-submit" value="<?php echo esc_attr_x( 'Search', 'submit button', 'gridd' ); ?>" />
+	</span>
+</form>
