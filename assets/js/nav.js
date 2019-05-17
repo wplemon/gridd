140ae72480a415235c59d615af5c471e2af70bc1
Ari <aristath@gmail.com>
2019-05-17 23:45:40 +0300

    a11y fix: close open submenus on tab-away. See https://themes.trac.wordpress.org/ticket/67531#comment:7

--- assets/js/nav.js
+++ assets/js/nav.js
@@ -1,8 +1,10 @@
 /**
  * Handle togglers.
  */
-var griddToggleButtonClick = function( id ) {
+function griddToggleButtonClick( id ) {
 	var el = document.querySelector( 'button[data-uid="' + id + '"]' );
+	griddNavToggleFocusByEl( el );
+    
 
     // Toggle the "toggled-on" class.
 	el.classList.toggle( 'toggled-on' );
@@ -14,3 +16,31 @@ var griddToggleButtonClick = function( id ) {
 		el.setAttribute( 'aria-expanded', 'false' );
 	}
 };
+
+window.onload = function() {
+	var griddNavLinks = document.querySelectorAll( '.menu-item a' ), i;
+	for ( i = 0; i < griddNavLinks.length; i++ ) {
+		griddNavLinks[ i ].addEventListener( 'focus', griddNavToggleFocus, true );
+		griddNavLinks[ i ].addEventListener( 'blur', griddNavToggleFocus, true );
+	}
+}
+
+function griddNavToggleFocus() {
+	griddNavToggleFocusByEl( this );
+}
+
+function griddNavToggleFocusByEl( el ) {
+	var isMenu = el.closest( '.menu' ), closestSubMenu, closestUl, allOpenSubMenuButtons, i;
+
+	if ( isMenu ) {
+		closestSubMenu        = el.closest( '.sub-menu' );
+		closestUl             = closestSubMenu ? closestSubMenu.closest( 'ul' ) : el.closest( 'ul' );
+		allOpenSubMenuButtons = closestUl.querySelectorAll( '.menu-item .gridd-toggle.toggled-on' );
+		for ( i = 0; i < allOpenSubMenuButtons.length; i++ ) {
+			if ( null === closestSubMenu || ( closestSubMenu && closestSubMenu.parentNode.querySelector( '.menu-item .gridd-toggle.toggled-on' ) !== allOpenSubMenuButtons[ i ] ) ) {
+				allOpenSubMenuButtons[ i ].classList.remove( 'toggled-on' );
+				allOpenSubMenuButtons[ i ].setAttribute( 'aria-expanded', 'false' );
+			}
+		}
+	}
+}
