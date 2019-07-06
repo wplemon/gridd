b3c040c09f9e866c8997a5f33dbaeac087411d90
Ari <aristath@gmail.com>
2019-07-06 13:20:06 +0300

    bugtfix for --gridd-content-max-width-calculated

--- grid-parts/scripts/content.js
+++ grid-parts/scripts/content.js
@@ -1,10 +1,15 @@
 var griddComputeEm = function() {
-	var root = document.querySelectorAll( ':root' )[0],
+	var root  = document.querySelectorAll( ':root' )[0],
 		style = getComputedStyle( document.body ),
-		em = style.getPropertyValue( 'font-size' ),
-		contentWidth = style.getPropertyValue( '--gridd-content-max-width' ),
-		maxWidthEm = -1 === contentWidth.indexOf( 'rem' ) && -1 !== contentWidth.indexOf( 'em' ),
-		width = maxWidthEm ? parseInt( parseFloat( contentWidth, 10 ) * parseFloat( em ), 10 ) + 'px' : contentWidth;
+		em    = style.getPropertyValue( 'font-size' ),
+		contentWidth,
+		maxWidthEm,
+		width;
+	
+	contentWidth = style.getPropertyValue( '--gridd-content-max-width' );
+	contentWidth = contentWidth && '' !== contentWidth ? contentWidth : '45em';
+	maxWidthEm   = -1 === contentWidth.indexOf( 'rem' ) && -1 !== contentWidth.indexOf( 'em' );
+	width        = maxWidthEm ? parseInt( parseFloat( contentWidth, 10 ) * parseFloat( em ), 10 ) + 'px' : contentWidth;
 
 	root.style.setProperty( '--gridd-em', em );
 	root.style.setProperty( '--gridd-content-max-width-calculated', width );
