68f5f331c61170c6c611407449e4d3e530c1fc1d
Ari Stathopoulos <aristath@gmail.com>
2019-08-10 00:22:40 +0300

    add css-vars polyfill

--- inc/classes/Scripts.php
+++ inc/classes/Scripts.php
@@ -116,6 +116,7 @@ class Scripts {
 			get_theme_file_path( 'assets/js/skip-link.min.js' ),
 			get_theme_file_path( 'assets/js/nav.min.js' ),
 			get_theme_file_path( 'assets/js/responsive-videos.min.js' ),
+			get_theme_file_path( 'assets/js/css-vars-polyfill.min.js' ),
 		];
 
 		$scripts = apply_filters( 'gridd_footer_inline_script_paths', $scripts );
