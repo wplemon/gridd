df2390ab0d9a784050e256e041c493c1b50a53af
Ari Stathopoulos <aristath@gmail.com>
2019-12-22 11:38:19 +0200

    header-contact-info refactor

--- grid-parts/Header/customizer-contact-info.php
+++ grid-parts/Header/customizer-contact-info.php
@@ -11,31 +11,77 @@ use Gridd\Customizer\Sanitize;
 
 $sanitization = new Sanitize();
 
-Customizer::add_outer_section(
+new \Kirki\Section(
 	'grid_part_details_header_contact_info',
 	[
 		'title' => esc_html__( 'Header Contact Info', 'gridd' ),
+		'panel' => 'theme_options',
+		'type'  => 'kirki-expanded',
+	]
+);
+
+new \Kirki\Field\Checkbox_Switch(
+	[
+		'settings'  => 'header_contact_info_custom_options',
+		'section'   => 'grid_part_details_header_contact_info',
+		'default'   => false,
+		'transport' => 'refresh',
+		'priority'  => -999,
+		'choices'   => [
+			'off' => esc_html__( 'Inherit Options', 'gridd' ),
+			'on'  => esc_html__( 'Override Options', 'gridd' ),
+		],
+	]
+);
+
+Customizer::add_field(
+	[
+		'type'              => 'textarea',
+		'settings'          => 'grid_part_details_header_contact_info',
+		'label'             => esc_html__( 'Content', 'gridd' ),
+		'description'       => Customizer::get_control_description(
+			[
+				'details' => esc_html__( 'Enter any text you want - usually your contact info or important announcements that you want your visitors to see.', 'gridd' ),
+			]
+		),
+		'section'           => 'grid_part_details_header_contact_info',
+		'default'           => __( 'Email: <a href="mailto:contact@example.com">contact@example.com</a>. Phone: +1-541-754-3010', 'gridd' ),
+		'transport'         => 'postMessage',
+		'js_vars'           => [
+			[
+				'element'  => '.gridd-tp-header_contact_info.gridd-tp',
+				'function' => 'html',
+			],
+		],
+		'choices'           => [
+			'language' => 'html',
+		],
+		'sanitize_callback' => 'wp_kses_post',
+		'priority'          => 10,
 	]
 );
 
 new \Kirki\Field\ReactColor(
 	[
-		'settings'  => 'header_contact_info_background_color',
-		'label'     => esc_html__( 'Background Color', 'gridd' ),
-		'section'   => 'grid_part_details_header_contact_info',
-		'default'   => '#ffffff',
-		'transport' => 'auto',
-		'output'    => [
+		'settings'        => 'header_contact_info_background_color',
+		'label'           => esc_html__( 'Background Color', 'gridd' ),
+		'section'         => 'grid_part_details_header_contact_info',
+		'default'         => '#ffffff',
+		'transport'       => 'auto',
+		'output'          => [
 			[
-				'element'  => '.gridd-tp-header_contact_info',
+				'element'  => '.gridd-tp-header_contact_info.custom-options',
 				'property' => '--bg',
 			],
 		],
-		'choices'   => [
+		'choices'         => [
 			'formComponent' => 'TwitterPicker',
 			'colors'        => \Gridd\Theme::get_colorpicker_palette(),
 		],
-		'priority'  => 20,
+		'priority'        => 20,
+		'active_callback' => function() {
+			return get_theme_mod( 'header_contact_info_custom_options', false );
+		},
 	]
 );
 
@@ -52,46 +98,52 @@ new \WPLemon\Field\WCAGTextColor(
 		'transport'         => 'auto',
 		'output'            => [
 			[
-				'element'  => '.gridd-tp-header_contact_info',
+				'element'  => '.gridd-tp-header_contact_info.custom-options',
 				'property' => '--cl',
 			],
 		],
 		'sanitize_callback' => [ $sanitization, 'color_hex' ],
 		'priority'          => 30,
+		'active_callback'   => function() {
+			return get_theme_mod( 'header_contact_info_custom_options', false );
+		},
 	]
 );
 
 Customizer::add_field(
 	[
-		'type'        => 'slider',
-		'settings'    => 'header_contact_info_font_size',
-		'label'       => esc_html__( 'Font Size', 'gridd' ),
-		'description' => esc_html__( 'The value selected here is relative to your body font-size, so a value of 1em will be the same size as your content.', 'gridd' ),
-		'section'     => 'grid_part_details_header_contact_info',
-		'default'     => .85,
-		'transport'   => 'auto',
-		'output'      => [
+		'type'            => 'slider',
+		'settings'        => 'header_contact_info_font_size',
+		'label'           => esc_html__( 'Font Size', 'gridd' ),
+		'description'     => esc_html__( 'The value selected here is relative to your body font-size, so a value of 1em will be the same size as your content.', 'gridd' ),
+		'section'         => 'grid_part_details_header_contact_info',
+		'default'         => .85,
+		'transport'       => 'auto',
+		'output'          => [
 			[
-				'element'  => '.gridd-tp-header_contact_info',
-				'property' => '--fs',
+				'element'  => '.gridd-tp-header_contact_info.custom-options',
+				'property' => '--fsr',
 			],
 		],
-		'choices'     => [
+		'choices'         => [
 			'min'    => .5,
 			'max'    => 2,
 			'step'   => .01,
 			'suffix' => 'em',
 		],
-		'priority'    => 40,
+		'priority'        => 40,
+		'active_callback' => function() {
+			return get_theme_mod( 'header_contact_info_custom_options', false );
+		},
 	]
 );
 
 Customizer::add_field(
 	[
-		'type'        => 'dimension',
-		'settings'    => 'header_contact_info_padding',
-		'label'       => esc_html__( 'Padding', 'gridd' ),
-		'description' => Customizer::get_control_description(
+		'type'            => 'dimension',
+		'settings'        => 'header_contact_info_padding',
+		'label'           => esc_html__( 'Padding', 'gridd' ),
+		'description'     => Customizer::get_control_description(
 			[
 				'short'   => '',
 				'details' => sprintf(
@@ -101,16 +153,19 @@ Customizer::add_field(
 				),
 			]
 		),
-		'section'     => 'grid_part_details_header_contact_info',
-		'default'     => '1em',
-		'transport'   => 'auto',
-		'output'      => [
+		'section'         => 'grid_part_details_header_contact_info',
+		'default'         => '1em',
+		'transport'       => 'auto',
+		'output'          => [
 			[
-				'element'  => '.gridd-tp-header_contact_info',
+				'element'  => '.gridd-tp-header_contact_info.custom-options',
 				'property' => '--pd',
 			],
 		],
-		'priority'    => 50,
+		'priority'        => 50,
+		'active_callback' => function() {
+			return get_theme_mod( 'header_contact_info_custom_options', false );
+		},
 	]
 );
 
@@ -136,33 +191,6 @@ new \Kirki\Field\RadioButtonset(
 			return ( 'flex-start' !== $value && 'flex-end' !== $value && 'center' !== $value ) ? 'flex-start' : $value;
 		},
 		'priority'          => 60,
-	]
-);
-
-Customizer::add_field(
-	[
-		'type'              => 'code',
-		'settings'          => 'grid_part_details_header_contact_info',
-		'label'             => esc_html__( 'Content', 'gridd' ),
-		'description'       => Customizer::get_control_description(
-			[
-				'details' => esc_html__( 'Enter any text you want - usually your contact info or important announcements that you want your visitors to see.', 'gridd' ),
-			]
-		),
-		'section'           => 'grid_part_details_header_contact_info',
-		'default'           => __( 'Email: <a href="mailto:contact@example.com">contact@example.com</a>. Phone: +1-541-754-3010', 'gridd' ),
-		'transport'         => 'postMessage',
-		'js_vars'           => [
-			[
-				'element'  => '.gridd-tp-header_contact_info.gridd-tp',
-				'function' => 'html',
-			],
-		],
-		'choices'           => [
-			'language' => 'html',
-		],
-		'sanitize_callback' => 'wp_kses_post',
-		'priority'          => 10,
 	]
 );
 
--- grid-parts/Header/template-contact-info.php
+++ grid-parts/Header/template-contact-info.php
@@ -14,9 +14,16 @@ $style->add_file( get_theme_file_path( 'grid-parts/Header/styles-contact-info.mi
 
 // Print styles.
 $style->the_css( 'gridd-inline-css-header-contact-info' );
+
+$attributes = [
+	'class' => 'gridd-tp gridd-tp-header_contact_info',
+];
+if ( get_theme_mod( 'header_contact_info_custom_options', false ) ) {
+	$attributes['class'] .= ' custom-options';
+}
 ?>
 
-<div <?php Theme::print_attributes( [ 'class' => 'gridd-tp gridd-tp-header_contact_info' ], 'wrapper-header_contact_info' ); ?>>
+<div <?php Theme::print_attributes( $attributes, 'wrapper-header_contact_info' ); ?>>
 	<?php
 	/**
 	 * Print the text entered by the user.
