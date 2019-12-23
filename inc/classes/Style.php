0b23de5818e83ebdc7efe6484d8f95ef56465a35
Konstantinos Xenos <mr.xkon@gmail.com>
2019-12-23 17:34:05 +0200

    pass true due to abspath

--- inc/classes/Style.php
+++ inc/classes/Style.php
@@ -227,7 +227,7 @@ class Style {
 	public function child_theme_styles() {
 		if ( is_child_theme() && apply_filters( 'gridd_load_child_theme_styles', true ) ) {
 			// Note to code reviewers: wp_strip_all_tags here is sufficient escape to ensure everything is interpreted as CSS.
-			echo '<style>' . wp_strip_all_tags( Theme::get_fcontents( get_stylesheet_directory() . '/style.css' ), true ) . '</style>'; // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped
+			echo '<style>' . wp_strip_all_tags( Theme::get_fcontents( get_stylesheet_directory() . '/style.css', true ), true ) . '</style>'; // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped
 		}
 	}
 }
