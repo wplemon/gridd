32598900cfeee65c0a0536b701322bdd1281abf4
Konstantinos Xenos <mr.xkon@gmail.com>
2019-12-23 17:41:34 +0200

    enqueue the child theme css only with content/part

--- inc/classes/Style.php
+++ inc/classes/Style.php
@@ -225,7 +225,8 @@ class Style {
 	 * @return void
 	 */
 	public function child_theme_styles() {
-		if ( is_child_theme() && apply_filters( 'gridd_load_child_theme_styles', true ) ) {
+		error_log( $this->context );
+		if ( is_child_theme() && apply_filters( 'gridd_load_child_theme_styles', true ) && 'grid-part/content' === $this->context ) {
 			// Note to code reviewers: wp_strip_all_tags here is sufficient escape to ensure everything is interpreted as CSS.
 			echo '<style>' . wp_strip_all_tags( Theme::get_fcontents( get_stylesheet_directory() . '/style.css', true ), true ) . '</style>'; // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped
 		}
