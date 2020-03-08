d30cb0767b905d5f6f75d1b23b5fe422e0a24716
Ari Stathopoulos <aristath@gmail.com>
2020-03-08 11:09:41 +0200

    widget-areas migrator tweaks

--- inc/classes/Upgrades/Block/Widget_Areas.php
+++ inc/classes/Upgrades/Block/Widget_Areas.php
@@ -33,84 +33,90 @@ class Widget_Areas {
 	 * @return void
 	 */
 	public function run() {
-		global $wp_registered_widgets;
 		// Get an array of all our widget areas.
 		$widget_areas = \wp_get_sidebars_widgets();
 
-		$registered_blocks = \WP_Block_Type_Registry::get_instance()->get_all_registered();
-
 		// Loop widget areas.
 		foreach ( $widget_areas as $id => $widgets ) {
-			if ( 'wp_inactive_widgets' === $id || empty( $widgets ) ) {
-				continue;
-			}
-			$can_migrate        = [];
-			$widgets_to_migrate = [];
 
-			foreach ( $widgets as $widget_id ) {
-
-				// Sanity check.
-				if (
-					! isset( $wp_registered_widgets[ $widget_id ] ) ||
-					! isset( $wp_registered_widgets[ $widget_id ]['callback'] ) ||
-					! is_array( $wp_registered_widgets[ $widget_id ]['callback'] ) ||
-					! isset( $wp_registered_widgets[ $widget_id ]['callback'][0] )
-				) {
-					$can_migrate[] = false;
-					continue;
-				}
-
-				$class = $wp_registered_widgets[ $widget_id ]['callback'][0];
-
-				if ( ! is_object( $class ) ) {
-					$can_migrate[] = false;
-					continue;
-				}
-
-				$widget_blocks = [
-					'WP_Widget_Archives'        => 'core/archives',
-					'WP_Widget_Media_Audio'     => 'core/audio',
-					'WP_Widget_Calendar'        => 'core/calendar',
-					'WP_Widget_Categories'      => 'core/categories',
-					'WP_Widget_Custom_HTML'     => 'core/html',
-					'WP_Widget_Media_Gallery'   => 'core/gallery',
-					'WP_Widget_Media_Image'     => 'core/image',
-					// 'WP_Widget_Meta' is currently unavailable.
-					// 'WP_Widget_Pages' is currently unavailable.
-					'WP_Widget_Recent_Comments' => 'core/latest-comments',
-					'WP_Widget_RSS'             => 'core/rss',
-					// 'WP_Nav_Menu_Widget' is not currently available.
-				];
-
-				// Check if this is a widget we can migrate.
-				$class_name = get_class( $class );
-
-				if ( isset( $widget_blocks[ $class_name ] ) ) {
-					if ( isset( $registered_blocks[ $widget_blocks[ $class_name ] ] ) ) {
-						$can_migrate[]        = true;
-						$widgets_to_migrate[] = $widget_id;
-					} else {
-						$can_migrate[] = false;
-					}
-				} else {
-					$can_migrate[] = false;
-				}
-			}
-
-			// Check if the sidebar can be migrated.
-			foreach ( $can_migrate as $check ) {
-				if ( ! $check ) {
-					$can_migrate = false;
-					break;
-				}
-			}
-
-			// Early exit if the sidebar can't be migrated.
-			if ( ! $can_migrate ) {
+			if ( ! $this->can_migrate_widget_area( $id, $widgets ) ) {
 				continue;
 			}
 
-			new \Gridd\Upgrades\Block\Widget_Area( [ $id, $widgets_to_migrate ] );
+			new \Gridd\Upgrades\Block\Widget_Area( [ $id, array_values( $widgets ) ] );
 		}
+	}
+
+	/**
+	 * Check if we can migrate a widget-area to blocks.
+	 *
+	 * @access protected
+	 * @since 3.0.0
+	 * @param string $id      The widget-area ID.
+	 * @param array  $widgets An array of widgets in the widget-area.
+	 * @return bool
+	 */
+	protected function can_migrate_widget_area( $id, $widgets ) {
+
+		if ( 'wp_inactive_widgets' === $id || empty( $widgets ) ) {
+			return false;
+		}
+		foreach ( $widgets as $widget_id ) {
+			$can_migrate_widget = $this->can_migrate_widget( $widget_id );
+			if ( ! $can_migrate_widget ) {
+				return false;
+			}
+		}
+		return true;
+	}
+
+	/**
+	 * Check if we can migrate a widget to block.
+	 *
+	 * @access protected
+	 * @since 3.0.0
+	 * @global $wp_registered_widgets
+	 * @param string $widget_id The widget-ID.
+	 * @return bool
+	 */
+	protected function can_migrate_widget( $widget_id ) {
+		global $wp_registered_widgets;
+		$registered_blocks = \WP_Block_Type_Registry::get_instance()->get_all_registered();
+
+		// Sanity check.
+		if (
+			! isset( $wp_registered_widgets[ $widget_id ] ) ||
+			! isset( $wp_registered_widgets[ $widget_id ]['callback'] ) ||
+			! is_array( $wp_registered_widgets[ $widget_id ]['callback'] ) ||
+			! isset( $wp_registered_widgets[ $widget_id ]['callback'][0] )
+		) {
+			return false;
+		}
+
+		$class = $wp_registered_widgets[ $widget_id ]['callback'][0];
+
+		if ( ! is_object( $class ) ) {
+			return false;
+		}
+
+		$widget_blocks = [
+			'WP_Widget_Archives'        => 'core/archives',
+			'WP_Widget_Media_Audio'     => 'core/audio',
+			'WP_Widget_Calendar'        => 'core/calendar',
+			'WP_Widget_Categories'      => 'core/categories',
+			'WP_Widget_Custom_HTML'     => 'core/html',
+			'WP_Widget_Media_Gallery'   => 'core/gallery',
+			'WP_Widget_Media_Image'     => 'core/image',
+			// 'WP_Widget_Meta' is currently unavailable.
+			// 'WP_Widget_Pages' is currently unavailable.
+			'WP_Widget_Recent_Comments' => 'core/latest-comments',
+			'WP_Widget_RSS'             => 'core/rss',
+			// 'WP_Nav_Menu_Widget' is not currently available.
+		];
+
+		// Check if this is a widget we can migrate.
+		$class_name = get_class( $class );
+
+		return ( isset( $widget_blocks[ $class_name ] ) && isset( $registered_blocks[ $widget_blocks[ $class_name ] ] ) );
 	}
 }
