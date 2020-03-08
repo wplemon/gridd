fd4ba1bc01ad5b6f7c3226ca53f42b4eb025986a
Ari Stathopoulos <aristath@gmail.com>
2020-03-08 11:33:44 +0200

    allow for dedicated editor block styles

--- inc/classes/Editor.php
+++ inc/classes/Editor.php
@@ -38,6 +38,9 @@ class Editor {
 		$styled_blocks = \Gridd\Block_Styles::get_styled_blocks();
 		foreach ( $styled_blocks as $block ) {
 			wp_enqueue_style( "gridd-editor-$block", get_template_directory_uri() . "/assets/css/blocks/$block.min.css", [], GRIDD_VERSION );
+			if ( file_exists( get_theme_file_path( "assets/css/blocks/$block-editor.min.css" ) ) ) {
+				wp_enqueue_style( "gridd-editor-$block-editor", get_template_directory_uri() . "/assets/css/blocks/$block-editor.min.css", [], GRIDD_VERSION );
+			}
 		}
 	}
 }
